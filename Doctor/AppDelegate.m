2fe251f63dea91ff735fa481abf50aa5523704f6
zt <sweet_tina_oo@163.com>
2019-01-09 22:56:52 +0800

    提交

--- Doctor/AppDelegate.m
+++ Doctor/AppDelegate.m
@@ -27,7 +27,6 @@
 #if DEBUG
     [[NSBundle bundleWithPath:@"/Applications/InjectionIII.app/Contents/Resources/iOSInjection10.bundle"] load];
 #endif
-    
     // 键盘配置
     IQKeyboardManager *keyManager = [IQKeyboardManager sharedManager];
     keyManager.enable = YES;
@@ -35,38 +34,29 @@
     keyManager.shouldToolbarUsesTextFieldTintColor = YES;
     keyManager.enableAutoToolbar = NO;
     keyManager.keyboardDistanceFromTextField = 60;
-    
     // 云信配置
     NIMSDKOption *option = [NIMSDKOption optionWithAppKey:@"47a23dbd2d737a031570d5a153a04a4e"];
     [[NIMSDK sharedSDK] registerWithOption:option];
-    
     // 初始化UI
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-
     if ([UserInfoManager shareInstance].user) {
         // 自动登录云信
         [[[NIMSDK sharedSDK] loginManager] autoLogin:[UserInfoManager shareInstance].user.accid
                                                token:[UserInfoManager shareInstance].user.tokenyx];
-        
         VHDTabbarVC *tabVC = [[VHDTabbarVC alloc] init];
         self.window.rootViewController = tabVC;
     } else {
         self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[LoginVC new]];
     }
-    
     [self.window makeKeyAndVisible];
-
-    
     return YES;
 }
-
 #pragma mark - 云信
 // 云信自动登录失败回调
 - (void)onAutoLoginFailed:(NSError *)error {
     if (!error) {
         return;
     }
-    
     [MBProgressHUD showLoadingWithTitle:@"账号异常，请重新登录"];
     [UIApplication sharedApplication].delegate.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[LoginVC new]];
     [[UserInfoManager shareInstance] logoutUser];
@@ -81,37 +71,26 @@
 
 // 当用户在某个客户端登录时，其他没有被踢掉的端会触发回调:
 - (void)onMultiLoginClientsChanged {
-    
-}
 
+}
 #pragma mark - 系统
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options NS_AVAILABLE_IOS(9_0){
     return YES;
 }
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+
 }
-
-
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
 }
-
-
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+
 }
-
-
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
 }
-
-
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
 }
 
 
